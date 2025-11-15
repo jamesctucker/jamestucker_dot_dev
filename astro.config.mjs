b41d3c6d613d81e9bbc20f97b9a9abb13ca31ced
James Tucker <hellojamestucker@gmail.com>
2025-11-15 16:32:33 -0600

    Add ssr to astro config for railway deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  server: {
+    host: "0.0.0.0",
+  },
+  adapter: node({
+    mode: "standalone",
+  }),
+});

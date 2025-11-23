ce8113991881d1c1138e45d85bf3996f5b49396a
James Tucker <hellojamestucker@gmail.com>
2025-11-22 22:11:22 -0600

    Setup initial style defaults

---
+++ src/content/config.ts
@@ -0,0 +1,6 @@
+// This file is required by Astro content collections
+// Even if you're not using collections yet, this prevents the error
+
+import { defineCollection } from "astro:content";
+
+export const collections = {};
